6bc9523b69c988df614a8c10eca662abc3915e7f
Filip Kovacevic <f.kovacevic@gmx.at>
2021-04-08 01:20:10 +0200

    added setup.py and configured meta.yaml

--- build.sh
+++ build.sh
@@ -1,10 +1,3 @@
-#!/bin/sh
+# Python command to install the script.
+$PYTHON setup.py install     
 
-# Variables
-# Actually $CONDA_PREFIX was intended to be used here but it seems that it is overwritten with $BUILD_PREFIX.
-dir_in_conda=$SYS_PREFIX/lib/python3.8/site-packages/$PKG_NAME 
-
-# Install
-echo "Start installation"
-# mkdir $dir_in_conda
-# cp -RT $RECIPE_DIR/$PKG_NAME $dir_in_conda
