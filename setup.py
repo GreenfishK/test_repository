dfdce35892d46fc03e3381b4fe3f80c00457b6d8
Filip Kovacevic <f.kovacevic@gmx.at>
2021-04-08 14:20:34 +0200

    removing everything from setup except name

--- setup.py
+++ setup.py
@@ -3,28 +3,28 @@ from setuptools import setup, find_packages
 setup(
     # Metadata
     name='my_test_pkg',
-    version='0.5.0',
-    author='Filip Kovacevic',
-    author_email='f.kovacevic@gmx.at',
-    license='GNU General Public License (GPL)',
-    description="A test package",
-    long_description="A test package long",
-    url='https://github.com/GreenfishK/test_repository',
+    #version='0.5.0',
+    #author='Filip Kovacevic',
+    #author_email='f.kovacevic@gmx.at',
+    #license='GNU General Public License (GPL)',
+    #description="A test package",
+    #long_description="A test package long",
+    #url='https://github.com/GreenfishK/test_repository',
 
     # Package data
-    packages=find_packages(include=['my_test_pkg']),
+    #packages=find_packages(include=['my_test_pkg']),
     # package_data={'my_test_package', ['*.db', '*.sql*, '*.txt.*'],
     # include_package_data=True,
 
     # Build and install Requirements
-    install_requires=[
-        'python>=3.8',
-        'matplotlib>=3.3.2',
-        'numpy >=1.18', 
-        'pandas >=1.1.2' 
-    ],
+    #install_requires=[
+    #    'python>=3.8',
+    #    'matplotlib>=3.3.2',
+    #    'numpy >=1.18', 
+    #    'pandas >=1.1.2' 
+    #],
     # extras_require={ 'interactive': ['matplotlib>=2.2.0,, 'jupyter'],
-    build=['python', 'conda', 'conda-build', 'anaconda-client', 'setuptools']  # 'twine', 'wheel'
+    # build=['python', 'conda', 'conda-build', 'anaconda-client', 'setuptools']  # 'twine', 'wheel'
 
     # Entry points
     # entry_points={'console_scripts': ['my-command=exampleproject.example:main']}
