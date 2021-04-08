bf09ae8670fe05d5d978b76f0924b9e7601e18de
Filip Kovacevic <f.kovacevic@gmx.at>
2021-04-08 17:25:55 +0200

    NEW approach with meta.yaml + setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,26 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-if __name__ == "__main__":
-    setup()
+setup(
+    # Metadata
+    name="my_test_pkg",
+    version="0.7.0",
+    author="Filip Kovacevic",
+    author_email="f.kovacevic@gmx.at",
+    description="A test description",
+    license="GPL",
+    keywords="example documentation tutorial",
+    url="http://packages.python.org/an_example_pypi_project",
+    long_description="A long description",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+    ],
+
+    # Packaging
+    packages=find_packages(
+        where='src',
+        include=['my_test_pkg'],
+    ),
+    package_dir={"": "src"}
+)
